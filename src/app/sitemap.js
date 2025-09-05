bb93c6aa9a05ed7ef677085b3c7cd8830a67e457
Muhammad Ubaidullah <ubaid56023@gmail.com>
2025-09-05 08:42:42 +0500

    sitemap fixes

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,18 +2,18 @@ export default async function sitemap() {
   const baseUrl = "https://www.akbartaxstore.com"; 
 
   const routes = [
-    { path: "/", priority: 1.0, changefreq: "weekly" },
-    { path: "/services-fees", priority: 0.9, changefreq: "monthly" },
-    { path: "/personal", priority: 0.8, changefreq: "monthly" },
-    { path: "/business", priority: 0.8, changefreq: "monthly" },
-    { path: "/about", priority: 0.6, changefreq: "yearly" },
-    { path: "/contact", priority: 0.7, changefreq: "yearly" }
+    { path: "/", priority: 1.0, changeFrequency: "weekly" },
+    { path: "/services-fees", priority: 0.9, changeFrequency: "monthly" },
+    { path: "/personal", priority: 0.8, changeFrequency: "monthly" },
+    { path: "/business", priority: 0.8, changeFrequency: "monthly" },
+    { path: "/about", priority: 0.6, changeFrequency: "yearly" },
+    { path: "/contact", priority: 0.7, changeFrequency: "yearly" }
   ];
 
   return routes.map((route) => ({
     url: `${baseUrl}${route.path}`,
     lastModified: new Date().toISOString(),
-    changefreq: route.changefreq,   
+    changeFrequency: route.changeFrequency,
     priority: route.priority,
   }));
 }
