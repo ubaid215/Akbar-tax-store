987bc93ac1e6daaea24da9c4bc5d7de378009ab2
Muhammad Ubaidullah <ubaid56023@gmail.com>
2026-06-05 07:33:58 +0500

    update layout with google analytics script file

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -1,5 +1,4 @@
-// src/app/(public)/layout.js
-
+import Script from 'next/script';
 import Navbar from '@/app/components/Navbar';
 import Footer from '@/app/components/Footer';
 import SmoothScrolling from '@/app/components/SmoothScrolling';
@@ -7,6 +6,20 @@ import SmoothScrolling from '@/app/components/SmoothScrolling';
 export default function PublicLayout({ children }) {
   return (
     <>
+      {/* Google Tag (gtag.js) */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
+
       <SmoothScrolling />
       <Navbar />
       <main className="pt-16 md:pt-24 lg:pt-20">
